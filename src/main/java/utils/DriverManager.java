10c338d7c4972a063f6b9f93b68593f3c9afbce9
OnchariWycliffe <onchariwycliffe.qa@gmail.com>
2019-09-10 10:24:47 +0300

    added Webdriver abstract method

---
+++ src/main/java/utils/DriverManager.java
@@ -0,0 +1,28 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+
+public abstract class DriverManager {
+
+	protected WebDriver driver;
+	
+	protected abstract void createWebDriver();
+	
+	
+	protected WebDriver getWebDriver() {
+		if(null == driver) {
+			createWebDriver();
+		}
+		return driver;
+	}
+	
+	public void quitWebDriver() {
+		if(null != driver) {
+			driver.quit();
+		}
+		
+		driver = null;
+	}
+}
+
+//https://www.logigear.com/blog/test-automation/building-a-selenium-framework-from-a-to-z/
